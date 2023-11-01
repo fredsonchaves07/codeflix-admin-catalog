201da127173fa01b7d46bafdad5e1b36747ff714
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-11-01 10:20:16 -0300

    feat: add beans genre use case
    Signed-off-by: fredsonchaves07 <fredson.ribeiro@objective.com.br>

---
+++ infrastructure/src/main/java/com/fredsonchaves/infraestructure/configuration/usecases/GenreUseCaseConfig.java
@@ -0,0 +1,49 @@
+package com.fredsonchaves.infraestructure.configuration.usecases;
+
+import com.fredsonchaves.application.genre.create.CreateGenreUseCase;
+import com.fredsonchaves.application.genre.delete.DeleteGenreUseCase;
+import com.fredsonchaves.application.genre.retrieve.get.GetGenreByIdUseCase;
+import com.fredsonchaves.application.genre.retrieve.list.GenreListUseCase;
+import com.fredsonchaves.application.genre.update.UpdateGenreUseCase;
+import com.fredsonchaves.domain.category.CategoryGateway;
+import com.fredsonchaves.domain.genre.GenreGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GenreUseCaseConfig {
+
+    private final GenreGateway genreGateway;
+
+    private final CategoryGateway categoryGateway;
+
+    public GenreUseCaseConfig(final GenreGateway genreGateway, final CategoryGateway categoryGateway) {
+        this.genreGateway = genreGateway;
+        this.categoryGateway = categoryGateway;
+    }
+
+    @Bean
+    public CreateGenreUseCase createGenreUseCase() {
+        return new CreateGenreUseCase(genreGateway, categoryGateway);
+    }
+
+    @Bean
+    public UpdateGenreUseCase updateGenreUseCase() {
+        return new UpdateGenreUseCase(categoryGateway, genreGateway);
+    }
+
+    @Bean
+    public GetGenreByIdUseCase getGenreByIdUseCase() {
+        return new GetGenreByIdUseCase(genreGateway);
+    }
+
+    @Bean
+    public GenreListUseCase genreListUseCase() {
+        return new GenreListUseCase(genreGateway);
+    }
+
+    @Bean
+    public DeleteGenreUseCase deleteGenreUseCase() {
+        return new DeleteGenreUseCase(genreGateway);
+    }
+}
