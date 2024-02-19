396e4ca3e8199ef97f56af9d3501965068c33b9c
fredsonchaves07 <fredsonchaves07@gmail.com>
2024-02-19 10:13:53 -0300

    feat: castmember validator
    Signed-off-by: fredsonchaves07 <fredson.ribeiro@objective.com.br>

--- domain/src/main/java/com/fredsonchaves/domain/castmember/CastMemberValidator.java
+++ domain/src/main/java/com/fredsonchaves/domain/castmember/CastMemberValidator.java
@@ -1,11 +1,16 @@
 package com.fredsonchaves.domain.castmember;
 
+import com.fredsonchaves.domain.validation.Error;
 import com.fredsonchaves.domain.validation.ValidationHandler;
 import com.fredsonchaves.domain.validation.Validator;
 
 public class CastMemberValidator extends Validator {
 
-    private final CastMember castMember;
+    public static final int NAME_MAX_LENGTH = 255;
+
+    public static final int NAME_MIN_LENGTH = 3;
+
+    private CastMember castMember;
 
     public CastMemberValidator(final CastMember castMember, final ValidationHandler handler) {
         super(handler);
@@ -14,6 +19,30 @@ public class CastMemberValidator extends Validator {
 
     @Override
     public void validate() {
+        checkNameConstraints();
+        checkTypeConstraints();
+    }
 
+    private void checkNameConstraints() {
+        final String name = castMember.getName();
+        if (name == null) {
+            validationHandler().append(new Error("'name' should not be null"));
+            return;
+        }
+        if (name.isBlank()) {
+            validationHandler().append(new Error("'name' should not be empty"));
+            return;
+        }
+        final int length = name.trim().length();
+        if (length > NAME_MAX_LENGTH || length < NAME_MIN_LENGTH) {
+            validationHandler().append(new Error("'name' must be between 3 and 255 characters"));
+        }
+    }
+
+    private void checkTypeConstraints() {
+        final var type = castMember.getType();
+        if (type == null) {
+            validationHandler().append(new Error("'type' should not be null"));
+        }
     }
 }
--- domain/src/test/java/com/fredsonchaves/domain/castmember/CastMemberTest.java
+++ domain/src/test/java/com/fredsonchaves/domain/castmember/CastMemberTest.java
@@ -1,8 +1,9 @@
 package com.fredsonchaves.domain.castmember;
 
+import com.fredsonchaves.domain.exceptions.NotificationException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CastMemberTest {
 
@@ -13,5 +14,63 @@ public class CastMemberTest {
         final var actualMember = CastMember.newMember(expectedName, expectedType);
         assertEquals(expectedName, actualMember.getName());
         assertEquals(expectedType, actualMember.getType());
+    }
+
+    @Test
+    public void givenAInvalidNullNameWhenCallsNewMemberShouldReceiveANotificacation() {
+        final var expectedType = CastMemberType.ACTOR;
+        final int expectedErrorCount = 1;
+        final String expectedErrorMessage = "'name' should not be null";
+        NotificationException notificationException = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(null, expectedType)
+        );
+        assertNotNull(notificationException);
+        assertEquals(expectedErrorCount, notificationException.getErrors().size());
+        assertEquals(expectedErrorMessage, notificationException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAInvalidEmptyNameWhenCallsNewMemberShouldReceiveANotificacation() {
+        final var expectedName = " ";
+        final var expectedType = CastMemberType.ACTOR;
+        final int expectedErrorCount = 1;
+        final String expectedErrorMessage = "'name' should not be empty";
+        NotificationException notificationException = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType)
+        );
+        assertNotNull(notificationException);
+        assertEquals(expectedErrorCount, notificationException.getErrors().size());
+        assertEquals(expectedErrorMessage, notificationException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAInvalidNameWithMoreThan255WhenCallsNewMemberShouldReceiveANotificacation() {
+        final var expectedName = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.";
+        final var expectedType = CastMemberType.ACTOR;
+        final int expectedErrorCount = 1;
+        final String expectedErrorMessage = "'name' must be between 3 and 255 characters";
+        NotificationException notificationException = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType)
+        );
+        assertNotNull(notificationException);
+        assertEquals(expectedErrorCount, notificationException.getErrors().size());
+        assertEquals(expectedErrorMessage, notificationException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAInvalidNullTypeNameWhenCallsNewMemberShouldReceiveANotificacation() {
+        final var expectedName = "Vin Dielsel";
+        final int expectedErrorCount = 1;
+        final String expectedErrorMessage = "'type' should not be null";
+        NotificationException notificationException = assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, null)
+        );
+        assertNotNull(notificationException);
+        assertEquals(expectedErrorCount, notificationException.getErrors().size());
+        assertEquals(expectedErrorMessage, notificationException.getErrors().get(0).message());
     }
 }
