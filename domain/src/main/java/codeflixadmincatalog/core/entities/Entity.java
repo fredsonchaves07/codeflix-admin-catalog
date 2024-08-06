0744487fcb4b9a4e03c9e0cfb54db1a164205c42
fredsonchaves07 <fredsonchaves07@gmail.com>
2024-08-06 18:25:26 -0300

    feat: core entity domains create

---
+++ domain/src/main/java/codeflixadmincatalog/core/entities/Entity.java
@@ -0,0 +1,40 @@
+package codeflixadmincatalog.core.entities;
+
+import java.time.LocalDateTime;
+
+public class Entity<ID extends Identifier> {
+
+    private final ID id;
+
+    private final LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    protected Entity(ID id) {
+        this.id = id;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public ID id() {
+        return id;
+    }
+
+    public LocalDateTime createdAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime updatedAt() {
+        return updatedAt;
+    }
+
+    public LocalDateTime deletedAt() {
+        return deletedAt;
+    }
+
+    public void update() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
