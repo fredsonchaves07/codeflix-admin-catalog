6004f086b7675940de518f6e45f15f7b6e868872
fredsonchaves07 <fredsonchaves07@gmail.com>
2024-08-07 09:41:03 -0300

    feat: make category with other parameters

--- domain/src/test/java/codeflixadmincatalog/factories/utils/MakeCategory.java
+++ domain/src/test/java/codeflixadmincatalog/factories/utils/MakeCategory.java
@@ -16,4 +16,28 @@ public class MakeCategory {
                 faker.bool().bool()
         );
     }
+
+    public static Category makeCategoryWithEmptyDescription() {
+        return Category.create(
+                faker.lorem().characters(50),
+                "",
+                faker.bool().bool()
+        );
+    }
+
+    public static Category makeCategoryWithNullDescription() {
+        return Category.create(
+                faker.lorem().characters(50),
+                null,
+                faker.bool().bool()
+        );
+    }
+
+    public static Category makeCategoryWithInactive() {
+        return Category.create(
+                faker.lorem().characters(7),
+                faker.lorem().characters(50),
+                false
+        );
+    }
 }
