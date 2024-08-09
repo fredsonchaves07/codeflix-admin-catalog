4a9870e966cf2c45f0cb05dd2d7a806a41f99fc9
fredsonchaves07 <fredsonchaves07@gmail.com>
2024-08-09 11:24:34 -0300

    feat: category validator

--- domain/src/main/java/codeflixadmincatalog/domain/entities/category/CategoryValidator.java
+++ domain/src/main/java/codeflixadmincatalog/domain/entities/category/CategoryValidator.java
@@ -2,6 +2,7 @@ package codeflixadmincatalog.domain.entities.category;
 
 import codeflixadmincatalog.core.entities.Validator;
 import codeflixadmincatalog.core.errors.DomainError;
+import codeflixadmincatalog.domain.errors.category.CategoryError;
 
 public class CategoryValidator implements Validator {
 
@@ -16,17 +17,17 @@ public class CategoryValidator implements Validator {
     }
 
     @Override
-    public void validate() throws DomainError {
+    public void validate() throws CategoryError {
         checkNameConstraints();
     }
 
     private void checkNameConstraints() {
         final String name = category.name();
         if (name == null)
-            throw DomainError.trows("Category name cannot be null");
+            throw CategoryError.trows("Category name cannot be null");
         if (name.isBlank())
-            throw DomainError.trows("Category name cannot be empty");
+            throw CategoryError.trows("Category name cannot be empty");
         if (name.trim().length() > MAX_NAME_LENGTH || name.trim().length() < MIN_NAME_LENGTH)
-            throw DomainError.trows("Category name must be between 3 to 50 characters");
+            throw CategoryError.trows("Category name must be between 3 to 50 characters");
     }
 }
