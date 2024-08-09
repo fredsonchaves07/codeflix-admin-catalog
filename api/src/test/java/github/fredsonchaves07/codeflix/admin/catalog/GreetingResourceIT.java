9c5686092e2f6ff6a6bd42836ccbb4e618dbced7
fredsonchaves07 <fredsonchaves07@gmail.com>
2024-08-09 11:21:53 -0300

    feat: api initial project version

---
+++ api/src/test/java/github/fredsonchaves07/codeflix/admin/catalog/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package github.fredsonchaves07.codeflix.admin.catalog;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
