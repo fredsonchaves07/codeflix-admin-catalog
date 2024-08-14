28149ce3e9ff2bf0262b81939dcae15880127771
fredsonchaves07 <fredsonchaves07@gmail.com>
2024-08-13 21:49:24 -0300

    feat: creation and organization of basic entity methods

--- domain/src/main/java/codeflixadmincatalog/core/entities/Entity.java
+++ domain/src/main/java/codeflixadmincatalog/core/entities/Entity.java
@@ -1,12 +1,14 @@
 package codeflixadmincatalog.core.entities;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
 
 public abstract class Entity<ID extends Identifier> {
 
     private final ID id;
 
-    private final LocalDateTime createdAt;
+    private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
 
@@ -14,8 +16,8 @@ public abstract class Entity<ID extends Identifier> {
 
     protected Entity(ID id) {
         this.id = id;
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now().truncatedTo(ChronoUnit.MICROS);
+        this.updatedAt = LocalDateTime.now().truncatedTo(ChronoUnit.MICROS);
     }
 
     public ID id() {
@@ -26,15 +28,40 @@ public abstract class Entity<ID extends Identifier> {
         return createdAt;
     }
 
+    public void createdAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
     public LocalDateTime updatedAt() {
         return updatedAt;
+    }
+
+    public void updatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 
     public LocalDateTime deletedAt() {
         return deletedAt;
     }
 
+    public void deletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
     public void update() {
-        this.updatedAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now().truncatedTo(ChronoUnit.MICROS);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entity<?> entity = (Entity<?>) o;
+        return Objects.equals(id, entity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
