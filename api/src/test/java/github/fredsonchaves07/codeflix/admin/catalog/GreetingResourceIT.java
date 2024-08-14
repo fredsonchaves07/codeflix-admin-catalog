03130c49b565720577c205bf067fdcd28f26b40f
fredsonchaves07 <fredsonchaves07@gmail.com>
2024-08-13 21:51:11 -0300

    chore: class not used for example only

--- api/src/test/java/github/fredsonchaves07/codeflix/admin/catalog/GreetingResourceIT.java
+++ api/src/test/java/github/fredsonchaves07/codeflix/admin/catalog/GreetingResourceIT.java
@@ -1,8 +1,8 @@
-package github.fredsonchaves07.codeflix.admin.catalog;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
+//package github.fredsonchaves07.codeflix.admin.catalog;
+//
+//import io.quarkus.test.junit.QuarkusIntegrationTest;
+//
+//@QuarkusIntegrationTest
+//class GreetingResourceIT extends GreetingResourceTest {
+//    // Execute the same tests but in packaged mode.
+//}
